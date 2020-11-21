f1bfc2b553211e6d1af3b7b552b3f4c29dbdbfd1
PabloMartinezAngerosa <pablomartinezangerosa@gmail.com>
2020-11-20 21:34:02 -0300

    Fix indentation

--- main.R
+++ main.R
@@ -1,3 +1,52 @@
 source("modelLM.R")
 
 
+#TODO ajustar el mismo modelo en ML Bayessiano
+#TODO model checking
+
+library(rstanarm)
+library(bayesplot)
+library(ggplot2)
+library(dplyr)
+
+
+modelo1 <- stan_glm(MORT ~ ., data = datosML,
+                    family = gaussian(link = "identity"),
+                    prior_intercept = normal(0,10),
+                    prior = normal(0,10),
+                    prior_aux = exponential(1),
+                    seed = 12345)
+
+summary(modelo1)
+prior_summary(modelo1)
+
+modelo1
+modelo1$coefficients
+modelo1$stanfit
+
+
+###Cadenas
+mcmc_trace(modelo1)
+mcmc_trace(modelo1,pars = "(Intercept)")
+mcmc_trace(modelo1,pars = "PREC")
+mcmc_trace(modelo1,pars = "JANT")
+mcmc_trace(modelo1,pars = "JULT")
+mcmc_trace(modelo1,pars = "EDUC")
+mcmc_trace(modelo1,pars = "NONW")
+mcmc_trace(modelo1,pars = "SO")
+mcmc_trace(modelo1,pars = "sigma")
+
+mcmc_intervals(modelo1 , prob = 0.95)##Intervalos de credibilidad del 95%
+
+mcmc_hist(modelo1)#Histograma de los coeficientes 
+mcmc_hist_by_chain(modelo1)#Histograma de los coeficientes por cadena
+mcmc_dens(modelo1)#Estimacion de densidad de los coeficientes
+
+
+##Predictivas posteriores
+pred<-posterior_predict(modelo1,draws = 100)
+
+pp_check(modelo1, plotfun = "stat", stat = "mean")##Usando un estadistico para comparaci+on
+pp_check(modelo1, plotfun = "stat", stat = "median")
+
+ppc_dens_overlay(datosML$MORT,pred)#Sobreposicion de las yrep con los datos originales
--- modelLM.R
+++ modelLM.R
@@ -15,7 +15,7 @@ base::dim(datos)
 base::summary(datos[,1:16])
 
 # Matriz de correlaciones
-base::round(stats::cor(datos[,1:16],use='pairwise.complete.obs') , 2) 
+base::round(stats::cor(datos[,1:16],use='pairwise.complete.obs'), 2) 
 
 # HC y NOX tienen correlacion de 0.98 se saca de la base NOX
 noxCol = base::which(base::names(datos) == "NOX")
@@ -24,11 +24,14 @@ datos = datos[,-noxCol]
 fittedGenearlLinearModel = stats::lm(MORT~. , data = datos)
 base::summary(fittedGenearlLinearModel) 
 
-# Analisis de Multicolinealidad, no hay valores >= 10 -> no hay multicolinealidad
+# Analisis de Multicolinealidad, no hay valores >= 10 
+# -> no hay multicolinealidad
 car::vif(fittedGenearlLinearModel)
 
 # Obtenemos modelo ajustados por step wise AIC
-stepWiseFitted = stats::step(fittedGenearlLinearModel, direction= "both", trace=0)
+stepWiseFitted = stats::step(fittedGenearlLinearModel, 
+                             direction= "both", 
+                             trace=0)
 
 # No se rechaza la normalidad
 nortest::lillie.test(stepWiseFitted$residuals) 
@@ -42,9 +45,13 @@ lmtest::bptest(stepWiseFitted)
 base::which(base::abs(stats::dffits(stepWiseFitted))>=2*base::sqrt(8/60))
 
 # Probamos sacar las influyentes y ver el R^2
-stepWiseFittedInf = statas::lm(formula = MORT ~ PREC + JANT + JULT + POPN + EDUC + NONW + SO, data = datos[-c(6, 28, 32, 37),])
+stepWiseFittedInf = stats::lm(formula = MORT ~ PREC + 
+                              JANT + JULT + POPN + 
+                              EDUC + NONW + SO, 
+                              data = datos[-c(6, 28, 32, 37),])
 
-# Analisis de Multicolinealidad, no hay valores >= 10 -> no hay multicolinealidad
+# Analisis de Multicolinealidad, no hay valores >= 10 
+# -> no hay multicolinealidad
 car::vif(stepWiseFittedInf)
 
 # No se rechaza la normalidad del modelo sin influyentes
@@ -59,12 +66,15 @@ lmtest::bptest(stepWiseFittedInf)
 car::outlierTest(stepWiseFittedInf)
 
 # Armamos el modelo nuevamente sin el atipico 
-fittedModel <- stats::lm(formula = MORT ~ PREC + JANT + JULT + POPN + EDUC + NONW + SO , datos[-c(6, 28, 32, 37, 2),])
+fittedModel <- stats::lm(formula = MORT ~ PREC + JANT + 
+                         JULT + POPN + EDUC + NONW + SO, 
+                         datos[-c(6, 28, 32, 37, 2),])
 
 # volvemos a correr el stepwise
 stepWiseFitted = stats::step(fittedModel, direction= "both", trace=0)
 
-# Analisis de Multicolinealidad, no hay valores >= 10 -> no hay multicolinealidad
+# Analisis de Multicolinealidad, no hay valores >= 10 
+# -> no hay multicolinealidad
 car::vif(stepWiseFitted)
 
 # No se rechaza la normalidad del modelo
@@ -77,6 +87,8 @@ lmtest::bptest(stepWiseFitted)
 
 # Seleccionamos el modelo
 modelML = stepWiseFitted
+datosML = datos[-c(6, 28, 32, 37, 2),]
+
 base::summary(modelML)
 
 
