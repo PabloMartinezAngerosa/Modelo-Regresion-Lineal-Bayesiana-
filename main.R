c07979d9c4804c18c83f86216a1a123cda8923d1
PabloMartinezAngerosa <pablomartinezangerosa@gmail.com>
2020-11-20 23:58:37 -0300

    cambios en indentacion

--- main.R
+++ main.R
@@ -9,8 +9,10 @@ library(bayesplot)
 library(ggplot2)
 library(dplyr)
 
-
-modelo1 <- stan_glm(MORT ~ ., data = datosML,
+#TODO probar prioris basada en ML
+modelo1 <- stan_glm(MORT ~ PREC + JANT + 
+                    JULT + EDUC + NONW + SO, 
+                    data = datosML,
                     family = gaussian(link = "identity"),
                     prior_intercept = normal(0,10),
                     prior = normal(0,10),
@@ -25,24 +27,31 @@ modelo1$coefficients
 modelo1$stanfit
 
 
-###Cadenas
-mcmc_trace(modelo1)
-mcmc_trace(modelo1,pars = "(Intercept)")
-mcmc_trace(modelo1,pars = "PREC")
-mcmc_trace(modelo1,pars = "JANT")
-mcmc_trace(modelo1,pars = "JULT")
-mcmc_trace(modelo1,pars = "EDUC")
-mcmc_trace(modelo1,pars = "NONW")
-mcmc_trace(modelo1,pars = "SO")
-mcmc_trace(modelo1,pars = "sigma")
+###Diagnostico de las Cadenas
+bayesplot::mcmc_trace(modelo1)
+bayesplot::mcmc_trace(modelo1,pars = "(Intercept)")
+bayesplot::mcmc_trace(modelo1,pars = "PREC")
+bayesplot::mcmc_trace(modelo1,pars = "JANT")
+bayesplot::mcmc_trace(modelo1,pars = "JULT")
+bayesplot::mcmc_trace(modelo1,pars = "EDUC")
+bayesplot::mcmc_trace(modelo1,pars = "NONW")
+bayesplot::mcmc_trace(modelo1,pars = "SO")
+bayesplot::mcmc_trace(modelo1,pars = "sigma")
 
-mcmc_intervals(modelo1 , prob = 0.95)##Intervalos de credibilidad del 95%
+##Intervalos de credibilidad del 95%
+mcmc_intervals(modelo1 ,pars=c("PREC",
+                               "JANT", 
+                               "JULT",
+                               "EDUC",
+                               "NONW",
+                               "SO"), prob = 0.95)
+
 
 mcmc_hist(modelo1)#Histograma de los coeficientes 
 mcmc_hist_by_chain(modelo1)#Histograma de los coeficientes por cadena
 mcmc_dens(modelo1)#Estimacion de densidad de los coeficientes
 
-
+# Diagnostico de Modelo
 ##Predictivas posteriores
 pred<-posterior_predict(modelo1,draws = 100)
 
